aac0207195dda7101dbb579441a313740935dab2
TheSukhSingh <130133318+TheSukhSingh@users.noreply.github.com>
2023-11-23 13:00:15 +0530

    .

--- Preparation/Python/Arithmetic Operators.py
+++
@@ -1,9 +0,0 @@
-if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    
-    c = a + b
-    d = a - b
-    e = a * b
-    
-    print(f'{c}\n{d}\n{e}')
--- Preparation/Python/Print Function.py
+++
@@ -1,6 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    
-    for i in range(n):
-        a = i + 1
-        print(a, end = "")
